c1da5eba84c57bff0a40ae8557e8c82627edd014
iamzilfa <iamzilfa@gmail.com>
2023-02-25 16:32:08 +0200

    configuring nextjs

---
+++ components/Layout/Navbar.jsx
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return (
+    <nav>
+        <div className="flex flex-col">
+            <div className="flex">
+                hey
+            </div>
+            <div></div>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
